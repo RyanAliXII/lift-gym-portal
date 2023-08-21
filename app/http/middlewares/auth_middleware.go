afbf71262a778bd82ae15b461de3dcc984bc1a3d
Ryan Ali <ryanali456@gmail.com>
2023-08-21 12:16:53 +0800

    handle response based on content-type header

--- app/http/middlewares/auth_middleware.go
+++ app/http/middlewares/auth_middleware.go
@@ -3,27 +3,42 @@ package middlewares
 import (
 	"net/http"
 
+	"slices"
+
 	"github.com/labstack/echo-contrib/session"
 	"github.com/labstack/echo/v4"
 )
 
 
-
-
-
 func AuthMiddleware(next echo.HandlerFunc) echo.HandlerFunc{
 	return func (c echo.Context) error {
+	
 		s , getSessionErr := session.Get("sid", c)
 		if getSessionErr != nil {
-			return c.Redirect(http.StatusFound, "/login")
+			s.Options.MaxAge = -1
+			s.Save(c.Request(), c.Response())
+			return handleResponse(c)
 		}
+		c.Set("test", "is the same")
 		if len(s.Values) ==  0{
 			s.Options.MaxAge = -1
 			s.Save(c.Request(), c.Response())
-			return c.Redirect(http.StatusFound, "/login")
+			return handleResponse(c)
 		}
 		return next(c)
 	}
+}
+
+func handleResponse(c echo.Context) error{
+	contentType := c.Request().Header.Get("content-type")
+	contentTypeWithJSONResponse := []string{"application/x-www-form-urlencoded,application/json"}
+	if(	slices.Contains(contentTypeWithJSONResponse, contentType)){
+		return c.JSON(http.StatusUnauthorized, map[string]interface{}{
+			"code": http.StatusUnauthorized,
+			"message": "Unauthorized.",
+		})
+	}	
+	return c.Redirect(http.StatusFound, "/login")
 }
 
 
--- handlers/register.go
+++ handlers/register.go
@@ -8,13 +8,19 @@ import (
 
 func RegisterHandlers(router *echo.Echo) {
 	loginHandler := NewLoginHandler()
-	dashboardHandler := NewDashboardHandler()
-	packageHandlder := NewPackageHandler()
+
 
 	
 	router.GET("/login", loginHandler.RenderLoginPage)
 	router.POST("/login", loginHandler.Login)
-	router.GET("/dashboard", dashboardHandler.RenderDashboardPage, middlewares.AuthMiddleware)
+	adminRoutes(router.Group(""))
+}
+
+func adminRoutes (router  * echo.Group){
+	dashboardHandler := NewDashboardHandler()
+	packageHandlder := NewPackageHandler()
+	router.Use(middlewares.AuthMiddleware)
+	router.GET("/dashboard", dashboardHandler.RenderDashboardPage,)
 	router.GET("/packages", packageHandlder.RenderPackagePage)
 	router.POST("/packages", packageHandlder.NewPackage)
 	router.PUT("/packages/:id", packageHandlder.UpdatePackage)
