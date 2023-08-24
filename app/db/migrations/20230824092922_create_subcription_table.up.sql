6146025319d19486fbae1bd5b2720b95286debd3
Ryan Ali <ryanali456@gmail.com>
2023-08-24 17:47:41 +0800

    change column name

--- app/db/migrations/20230824092922_create_subcription_table.up.sql
+++ app/db/migrations/20230824092922_create_subcription_table.up.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS subscription(
     id INT NOT NULL AUTO_INCREMENT  PRIMARY KEY,
     client_id INT NOT NULL,
     membership_plan_id INT NOT NULL,
-    validity DATE NOT NULL,
+    valid_until DATE NOT NULL,
     cancelled_at TIMESTAMP,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
