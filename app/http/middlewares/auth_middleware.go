61851402e93a78b2fba726ba55db2650ef8ac25f
Ryan Ali <ryanali456@gmail.com>
2023-08-26 16:33:54 +0800

    Implement login and create auth middleware for client

--- app/http/middlewares/auth_middleware.go
+++ app/http/middlewares/auth_middleware.go
@@ -19,11 +19,28 @@ func AuthMiddleware(next echo.HandlerFunc) echo.HandlerFunc{
 			s.Save(c.Request(), c.Response())
 			return handleResponse(c)
 		}
-		c.Set("test", "is the same")
 		if len(s.Values) ==  0{
 			s.Options.MaxAge = -1
 			s.Save(c.Request(), c.Response())
 			return handleResponse(c)
+		}
+		return next(c)
+	}
+}
+
+func ClientAuthMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		
+		s , getSessionErr := session.Get("client_sid", c)
+		if getSessionErr != nil {
+			s.Options.MaxAge = -1
+			s.Save(c.Request(), c.Response())
+			return handleResponseClient(c)
+		}
+		if len(s.Values) ==  0{
+			s.Options.MaxAge = -1
+			s.Save(c.Request(), c.Response())
+			return handleResponseClient(c)
 		}
 		return next(c)
 	}
@@ -38,6 +55,18 @@ func handleResponse(c echo.Context) error{
 		})
 	}	
 	return c.Redirect(http.StatusFound, "/app/login")
+}
+
+func handleResponseClient(c echo.Context) error{
+	contentType := c.Request().Header.Get("content-type")
+	contentTypeWithJSONResponse := []string{"application/x-www-form-urlencoded,application/json"}
+	if(	slices.Contains(contentTypeWithJSONResponse, contentType)){
+		return c.JSON(http.StatusUnauthorized, map[string]interface{}{
+			"code": http.StatusUnauthorized,
+			"message": "Unauthorized.",
+		})
+	}	
+	return c.Redirect(http.StatusFound, "/clients/login")
 }
 
 
--- app/repository/user.repo.go
+++ app/repository/user.repo.go
@@ -18,7 +18,14 @@ func (repo *UserRepository) GetUserByEmail(email string)(model.User, error) {
 	return user, getErr
 
 }
-
+func (repo *UserRepository) GetClientUserByEmail (email string) (model.User, error){
+	user := model.User{}
+	selectQuery := `SELECT client.id, client.given_name,client.middle_name,client.surname, account.email, account.password FROM client 
+	INNER JOIN account on client.account_id = account.id where UPPER(account.email) = UPPER(?) LIMIT 1;
+	`
+	getErr := repo.db.Get(&user, selectQuery, email)
+	return user, getErr
+}
 func NewUserRepository () UserRepository{
 	return UserRepository{
 		db: db.GetConnection(),
