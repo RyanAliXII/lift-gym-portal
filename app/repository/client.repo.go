747c4131e9cfdd52fa9b80ae4198b11f83cf0198
Ryan Ali <ryanali456@gmail.com>
2023-08-27 03:25:59 +0800

    add IsVerified property

--- app/repository/client.repo.go
+++ app/repository/client.repo.go
@@ -45,8 +45,8 @@ func (repo * ClientRepository)Get()([]model.Client, error) {
 }
 func (repo * ClientRepository)GetById(id int)(model.Client, error) {
 	clients := model.Client{}
-	selectQuery := `SELECT client.id, client.given_name, client.middle_name, client.surname, client.date_of_birth, client.address, client.emergency_contact,client.mobile_number, account.email, account.id as account_id from client
-	INNER JOIN account on client.account_id = account.id where client.id = ? ORDER BY client.updated_at DESC LIMIT 1`
+	selectQuery := `SELECT client.id, client.given_name, client.middle_name, client.surname, client.date_of_birth, client.address, client.emergency_contact,client.mobile_number, account.email, account.id as account_id, (case when verified_at is null then false else true end ) as is_verified from client
+	INNER JOIN account on client.account_id = account.id where client.id = ? ORDER BY client.updated_at DESC LIMIT 1;`
 	getErr := repo.db.Get(&clients, selectQuery , id)
 	return clients, getErr 
 }
