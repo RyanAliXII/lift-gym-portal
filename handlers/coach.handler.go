bfd99d5a1c0e39fada6a74bf35a2d633f4ce85f4
Ryan Ali <ryanali456@gmail.com>
2023-08-28 22:15:07 +0800

    create coach model with validation and create handler function for coach creation

--- handlers/coach.handler.go
+++ handlers/coach.handler.go
@@ -17,13 +17,22 @@ func (h *CoachHandler) RenderCoachPage(c echo.Context) error {
 		"csrf": csrf,
 	})
 }
-func (h  CoachHandler)RenderCoachRegistrationPage(c echo.Context) error {
+func (h * CoachHandler)RenderCoachRegistrationPage(c echo.Context) error {
 	csrf := c.Get("csrf")
 	return c.Render(http.StatusOK, "admin/coach/register-coach", Data{
 		"csrf": csrf,
 		"title": "Coach | Registration",
 		"module": "Registration Form",
 	} )
+}
+
+
+func (h  *CoachHandler)NewCoach (c echo.Context) error {
+	return c.JSON(http.StatusOK, JSONResponse{
+			Status: http.StatusOK,
+			Data: nil,
+			Message: "Coach has been registered.",
+	})
 }
 
 func NewCoachHandler() CoachHandler{
