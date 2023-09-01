2d8c9b6fbaa7343d678d1ea62d71e3cf7bc799d6
Ryan Ali <ryanali456@gmail.com>
2023-09-01 12:43:52 +0800

    create a function that fetches unrequested membership plans for specific client

--- app/repository/membership_plan.repo.go
+++ app/repository/membership_plan.repo.go
@@ -16,15 +16,20 @@ func (repo * MembershipPlanRepository) New(plan model.MembershipPlan) error{
 	return insertErr
 }
 
-func (repo  MembershipPlanRepository)Get() ([]model.MembershipPlan, error) {
+func (repo  *MembershipPlanRepository)Get() ([]model.MembershipPlan, error) {
 	plans := make([]model.MembershipPlan, 0)
 	selectErr := repo.db.Select(&plans, "SELECT id, description, price, months from membership_plan order by updated_at desc")
 	return plans, selectErr
 
 }
-func (repo  MembershipPlanRepository)Update(plan model.MembershipPlan)(error) {
+func (repo  *MembershipPlanRepository)Update(plan model.MembershipPlan)(error) {
 	_, updateErr := repo.db.Exec(`UPDATE membership_plan SET description = ?, price = ?, months = ? WHERE id = ?`, plan.Description, plan.Price, plan.Months, plan.Id )
 	return updateErr
+}
+func (repo * MembershipPlanRepository)GetUnrequestedPlansOfClient(clientId int)([]model.MembershipPlan, error	){
+	plans := make([]model.MembershipPlan, 0)
+	err := repo.db.Select(&plans, `SELECT  id, description, price, months FROM membership_plan as mp where mp.id NOT IN(SELECT membership_request.membership_plan_id FROM membership_request where membership_request.client_id = ?)`)
+	return plans, err
 }
 func NewMembershipPlanRepository() MembershipPlanRepository{
 	return MembershipPlanRepository{
