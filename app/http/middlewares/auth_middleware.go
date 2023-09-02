0066f237d7aeafe92b3a21383f1df78b74c2dc31
Ryan Ali <ryanali456@gmail.com>
2023-09-03 02:09:15 +0800

    refactor auth middleware

--- app/http/middlewares/auth_middleware.go
+++ app/http/middlewares/auth_middleware.go
@@ -10,59 +10,27 @@ import (
 )
 
 
-func AuthMiddleware(next echo.HandlerFunc) echo.HandlerFunc{
-	return func (c echo.Context) error {
-		s , getSessionErr := session.Get("sid", c)
-		if getSessionErr != nil {
-			s.Options.MaxAge = -1
-			s.Save(c.Request(), c.Response())
-			return handleResponse(c)
+func AuthMiddleware ( sessionCookieName string, redirectHereOnFail string) echo.MiddlewareFunc{
+	return func(next echo.HandlerFunc) echo.HandlerFunc{
+		return func (c echo.Context) error {
+			s , getSessionErr := session.Get("sid", c)
+			if getSessionErr != nil {
+				s.Options.MaxAge = -1
+				s.Save(c.Request(), c.Response())
+				return handleResponse(c, redirectHereOnFail )
+			}
+			if len(s.Values) ==  0{
+				s.Options.MaxAge = -1
+				s.Save(c.Request(), c.Response())
+				return handleResponse(c, redirectHereOnFail)
+			}
+			c.Set("sessionData", s.Values["data"])
+			return next(c)
 		}
-		if len(s.Values) ==  0{
-			s.Options.MaxAge = -1
-			s.Save(c.Request(), c.Response())
-			return handleResponse(c)
-		}
-		c.Set("sessionData", s.Values["data"])
-		return next(c)
 	}
 }
 
-func ClientAuthMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
-	return func(c echo.Context) error {
-		s , getSessionErr := session.Get("client_sid", c)
-		if getSessionErr != nil {
-			s.Options.MaxAge = -1
-			s.Save(c.Request(), c.Response())
-			return handleResponseClient(c)
-		}
-		if len(s.Values) ==  0{
-			s.Options.MaxAge = -1
-			s.Save(c.Request(), c.Response())
-			return handleResponseClient(c)
-		}
-		c.Set("sessionData", s.Values["data"])
-		return next(c)
-	}
-}
-func CoachAuthMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
-	return func(c echo.Context) error {
-		s , getSessionErr := session.Get("coach_sid", c)
-		if getSessionErr != nil {
-			s.Options.MaxAge = -1
-			s.Save(c.Request(), c.Response())
-			return handleResponseCoach(c)
-		}
-		if len(s.Values) ==  0{
-			s.Options.MaxAge = -1
-			s.Save(c.Request(), c.Response())
-			return handleResponseCoach(c)
-		}
-		c.Set("sessionData", s.Values["data"])
-		return next(c)
-	}
-}
-func handleResponse(c echo.Context) error{
+func handleResponse(c echo.Context, redirectTo string) error{
 	contentType := c.Request().Header.Get("content-type")
 	contentTypeWithJSONResponse := []string{"application/x-www-form-urlencoded,application/json"}
 	if(	slices.Contains(contentTypeWithJSONResponse, contentType)){
@@ -71,32 +39,7 @@ func handleResponse(c echo.Context) error{
 			"message": "Unauthorized.",
 		})
 	}	
-	return c.Redirect(http.StatusFound, "/app/login")
+	return c.Redirect(http.StatusSeeOther, redirectTo)
 }
-
-func handleResponseClient(c echo.Context) error{
-	contentType := c.Request().Header.Get("content-type")
-	contentTypeWithJSONResponse := []string{"application/x-www-form-urlencoded,application/json"}
-	if(	slices.Contains(contentTypeWithJSONResponse, contentType)){
-		return c.JSON(http.StatusUnauthorized, map[string]interface{}{
-			"code": http.StatusUnauthorized,
-			"message": "Unauthorized.",
-		})
-	}	
-	return c.Redirect(http.StatusFound, "/clients/login")
-}
-func handleResponseCoach(c echo.Context) error{
-	contentType := c.Request().Header.Get("content-type")
-	contentTypeWithJSONResponse := []string{"application/x-www-form-urlencoded,application/json"}
-	if(	slices.Contains(contentTypeWithJSONResponse, contentType)){
-		return c.JSON(http.StatusUnauthorized, map[string]interface{}{
-			"code": http.StatusUnauthorized,
-			"message": "Unauthorized.",
-		})
-	}	
-	return c.Redirect(http.StatusFound, "/coaches/login")
-}
-
-
 
 
