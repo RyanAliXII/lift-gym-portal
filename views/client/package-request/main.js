ff29d5324d7c05e3f63175722aa99c803f0b6606
Ryan Ali <ryanali456@gmail.com>
2023-09-04 22:18:24 +0800

    initialize package selection

--- views/client/package-request/main.js
+++ views/client/package-request/main.js
@@ -1,7 +1,18 @@
-import { createAppm, ref } from "vue";
+import { createApp, onMounted, ref } from "vue";
+import Choices from "choices.js";
 
 createApp({
   setup() {
     const packageSelectElement = ref(null);
+    let packageSelect = null;
+    const errorMessage = ref(undefined);
+    onMounted(() => {
+      packageSelect = new Choices(packageSelectElement.value, {});
+    });
+
+    return {
+      packageSelectElement,
+      errorMessage,
+    };
   },
 }).mount("#PackageRequest");
