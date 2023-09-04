496a8c20491cdbe13f987a1a1d8dd62ff35fe58d
Ryan Ali <ryanali456@gmail.com>
2023-09-04 18:38:10 +0800

    update validation error message of client model and add date of birth validation

--- app/model/client.model.go
+++ app/model/client.model.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"lift-fitness-gym/app/db"
+	"time"
 
 	validation "github.com/go-ozzo/ozzo-validation"
 	"github.com/go-ozzo/ozzo-validation/is"
@@ -31,34 +32,44 @@ func (m Client) Validate() (error, map[string]string) {
 
 	db := db.GetConnection()
 	return m.Model.ValidationRules(&m, 
-		validation.Field(&m.GivenName, validation.Required, validation.Length(1, 255)), 
-		validation.Field(&m.MiddleName, validation.Required, validation.Length(1, 255)),
-		validation.Field(&m.Surname, validation.Required, validation.Length(1, 255)),
-		validation.Field(&m.Address, validation.Required),
-		validation.Field(&m.Email, validation.Required, validation.Length(1, 255), is.Email, validation.By(func(value interface{}) error {
+		validation.Field(&m.GivenName, validation.Required.Error("Given name is required."), validation.Length(1, 255).Error("Given name is required.")), 
+		validation.Field(&m.MiddleName, validation.Required.Error("Middle name is required."), validation.Length(1, 255).Error("Middle name is required.")), 
+		validation.Field(&m.Surname, validation.Required.Error("Surname is required."), validation.Length(1, 255).Error("Surname is required.")), 
+		validation.Field(&m.DateOfBirth, validation.Required.Error("Date of birth is required."), validation.By(func(value interface{}) error {
+			format := "2006-01-02"
+			strDate ,_ := value.(string)
+			_, err := time.Parse(format, strDate)
+			if err != nil {
+				return fmt.Errorf("Date of birth is required.")
+			}
+			return nil
+		})),
+		validation.Field(&m.Address, validation.Required.Error("Address is required."), validation.Length(1, 255).Error("Address")),
+		validation.Field(&m.Email, validation.Required.Error("Email is required."), validation.Length(1, 255).Error("Email is required."), is.Email.Error("Invalid email"), validation.By(func(value interface{}) error {
 			recordCount := 0
 			query := `SELECT COUNT(1) as record_count from client
 			INNER JOIN account on client.account_id = account.id where UPPER(account.email) = UPPER(?) LIMIT 1;`
 			db.Get(&recordCount, query, m.Email)
 			if recordCount > 0 {
-				return fmt.Errorf("email is already registered")
+				return fmt.Errorf("Email is already registered")
 			}
 		
 			return nil
 		})),	
-	    validation.Field(&m.MobileNumber, validation.Required, validation.By(func(value interface{}) error {
+	    validation.Field(&m.MobileNumber, validation.Required.Error("Mobile number is required."), validation.By(func(value interface{}) error {
 			p, _ := phonenumbers.Parse(m.MobileNumber, "PH")
 			isValid := phonenumbers.IsValidNumberForRegion(p, "PH")
 			if !isValid {
-				return fmt.Errorf("invalid number")
+				return fmt.Errorf("Invalid number")
 			}
 			return nil
-		 })), 
-		validation.Field(&m.EmergencyContact, validation.Required, validation.By(func(value interface{}) error {
+		 })),
+ 
+		validation.Field(&m.EmergencyContact, validation.Required.Error("Emergency contact number is required."), validation.By(func(value interface{}) error {
 			p, _ := phonenumbers.Parse(m.EmergencyContact, "PH")
 			isValid := phonenumbers.IsValidNumberForRegion(p, "PH")
 			if !isValid {
-				return fmt.Errorf("invalid number")
+				return fmt.Errorf("Invalid number")
 			}
 			return nil})))
 }
@@ -66,33 +77,42 @@ func (m Client) ValidateUpdate() (error, map[string]string) {
 
 	db := db.GetConnection()
 	return m.Model.ValidationRules(&m, 
-		validation.Field(&m.GivenName, validation.Required, validation.Length(1, 255)), 
-		validation.Field(&m.MiddleName, validation.Required, validation.Length(1, 255)),
-		validation.Field(&m.Surname, validation.Required, validation.Length(1, 255)),
-		validation.Field(&m.Address, validation.Required),
-		validation.Field(&m.Email, validation.Required, validation.Length(1, 255), is.Email, validation.By(func(value interface{}) error {
+		validation.Field(&m.GivenName, validation.Required.Error("Given name is required."), validation.Length(1, 255).Error("Given name is required.")), 
+		validation.Field(&m.MiddleName, validation.Required.Error("Middle name is required."), validation.Length(1, 255).Error("Middle name is required.")), 
+		validation.Field(&m.Surname, validation.Required.Error("Surname is required."), validation.Length(1, 255).Error("Surname is required.")), 
+		validation.Field(&m.DateOfBirth, validation.Required.Error("Date of birth is required."), validation.By(func(value interface{}) error {
+			format := "2006-01-02"
+			strDate ,_ := value.(string)
+			_, err := time.Parse(format, strDate)
+			if err != nil {
+				return fmt.Errorf("Date of birth is required.")
+			}
+			return nil
+		})),
+		validation.Field(&m.Address, validation.Required.Error("Address is required."), validation.Length(1, 255).Error("Address")),
+		validation.Field(&m.Email, validation.Required.Error("Email is required."), validation.Length(1, 255).Error("Email is required."), is.Email.Error("Invalid email"), validation.By(func(value interface{}) error {
 			recordCount := 0
 			query := `SELECT COUNT(1) as record_count from client
 			INNER JOIN account on client.account_id = account.id where UPPER(account.email) = UPPER(?) and client.id != ? LIMIT 1;`
 			db.Get(&recordCount, query, m.Email, m.Id)
 			if recordCount > 0 {
-				return fmt.Errorf("email is already registered")
+				return fmt.Errorf("Email is already registered.")
 			}		
 			return nil
 		})),	
-	    validation.Field(&m.MobileNumber, validation.Required, validation.By(func(value interface{}) error {
+	    validation.Field(&m.MobileNumber, validation.Required.Error("Mobile number is required."), validation.By(func(value interface{}) error {
 			p, _ := phonenumbers.Parse(m.MobileNumber, "PH")
 			isValid := phonenumbers.IsValidNumberForRegion(p, "PH")
 			if !isValid {
-				return fmt.Errorf("invalid number")
+				return fmt.Errorf("Invalid number")
 			}
 			return nil
 		 })), 
-		validation.Field(&m.EmergencyContact, validation.Required, validation.By(func(value interface{}) error {
+		validation.Field(&m.EmergencyContact, validation.Required.Error("Emergency contact number is required."), validation.By(func(value interface{}) error {
 			p, _ := phonenumbers.Parse(m.EmergencyContact, "PH")
 			isValid := phonenumbers.IsValidNumberForRegion(p, "PH")
 			if !isValid {
-				return fmt.Errorf("invalid number")
+				return fmt.Errorf("Invalid number")
 			}
 			return nil})))
 }
