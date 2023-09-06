9542f59e4a623d87b6644e04966141ae2530f353
Ryan Ali <ryanali456@gmail.com>
2023-09-06 21:31:34 +0800

    add model validation on update

--- app/model/staff.model.go
+++ app/model/staff.model.go
@@ -35,3 +35,22 @@ func (m Staff) Validate() (error, map[string]string) {
 		})),
 	)
 }
+func (m Staff) ValidateUpdate() (error, map[string]string) {
+	db := db.GetConnection()
+	return m.Model.ValidationRules(&m, 
+		validation.Field(&m.GivenName, validation.Required.Error("Given name is required."), validation.Length(1, 255).Error("Given name must be 1 to 255 characters.")),
+		validation.Field(&m.Surname, validation.Required.Error("Surname is required."), validation.Length(1, 255).Error("Surname must be 1 to 255 characters.")),
+		validation.Field(&m.MiddleName, validation.Required.Error("Middle name is required."), validation.Length(1, 255).Error("Middle name must be 1 to 255 characters.")),
+		validation.Field(&m.Email, validation.Required.Error("Email is required."), validation.Length(1, 255).Error("Email must be 1 to 255 characters"), is.Email.Error("Invalid email"), validation.By(func(value interface{}) error {
+			recordCount := 0
+			query := `SELECT COUNT(1) as record_count from user
+			INNER JOIN account on user.account_id = account.id where UPPER(account.email) = UPPER(?) AND user.id != ? LIMIT 1;`
+			db.Get(&recordCount, query, m.Email, m.Id)
+			if recordCount > 0 {
+				return fmt.Errorf("Email is already registered.")
+			}		
+			return nil
+		})),
+	)
+}
+
