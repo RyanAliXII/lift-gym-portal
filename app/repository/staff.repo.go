5c4558657f766c4a85804a436abfdada02c2ec94
Ryan Ali <ryanali456@gmail.com>
2023-09-06 20:45:46 +0800

    create a staff repository

---
+++ app/repository/staff.repo.go
@@ -0,0 +1,43 @@
+package repository
+
+import (
+	"lift-fitness-gym/app/db"
+	"lift-fitness-gym/app/model"
+
+	"github.com/jmoiron/sqlx"
+)
+
+type StaffRepository struct {
+	db * sqlx.DB
+}
+
+func (repo * StaffRepository) NewStaff(staff model.Staff) error{
+	transaction, err := repo.db.Begin()
+	if err  != nil {
+		transaction.Rollback()
+		return err
+	}
+	insertAccountQuery := `INSERT INTO account(email, password) VALUES(?, ?, ?)`
+	result, err := transaction.Exec(insertAccountQuery, staff.Email, staff.Password)
+	if err != nil {
+		transaction.Rollback()
+		return err
+	}
+	accountId, lastInsertedIdErr := result.LastInsertId()
+	if lastInsertedIdErr != nil {
+		transaction.Rollback()
+		return err
+	}
+	insertUserQuery := `INSERT INTO user(given_name, middle_name, surname, account_id) VALUES(?, ?, ?, ?)`
+	_, err = transaction.Exec(insertUserQuery, staff.GivenName, staff.MiddleName, staff.Surname, accountId)
+	if err != nil {
+		transaction.Rollback()
+		return err
+	}
+	return nil	
+}
+func NewStaffRepository()StaffRepository{
+	return StaffRepository{
+		db: db.GetConnection(),
+	}
+}
