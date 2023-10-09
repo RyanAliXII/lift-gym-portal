8652ea28050fdd833e4edff710421759d31aa19e
Ryan Ali <ryanali456@gmail.com>
2023-10-09 18:49:26 +0800

    create add modal and initialize vue

--- views/admin/workouts/category/main.js
+++ views/admin/workouts/category/main.js
@@ -1,5 +1,19 @@
+import { useForm } from "vee-validate";
+import { createApp } from "vue";
+import { object } from "yup";
 
-
-
-
-
+createApp({
+  setup() {
+    const { values, defineInputBinds } = useForm({
+      initialValues: {
+        id: 0,
+        name: "",
+      },
+      validationSchema: object({}),
+    });
+    const name = defineInputBinds("name", { validateOnChange: true });
+    return {
+      name,
+    };
+  },
+}).mount("#WorkoutCategoryPage");
