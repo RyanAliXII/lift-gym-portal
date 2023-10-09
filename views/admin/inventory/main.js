2d794a7590dbbbd97f4e7b6d3562409470b31129
Ryan Ali <ryanali456@gmail.com>
2023-10-09 15:14:07 +0800

    bind form in useForm

--- views/admin/inventory/main.js
+++ views/admin/inventory/main.js
@@ -4,11 +4,7 @@ import { format } from "date-fns";
 import { object } from "yup";
 createApp({
   setup() {
-    const {
-      values: form,
-      errors,
-      defineInputBinds,
-    } = useForm({
+    const { values, errors, defineInputBinds, validate } = useForm({
       initialValues: {
         name: "",
         model: "",
@@ -18,29 +14,18 @@ createApp({
       },
       validationSchema: object({}),
     });
-    const name = defineInputBinds(
-      { value: "name" },
-      { validateOnChange: true }
-    );
-    const model = defineInputBinds(
-      { value: "model" },
-      {
-        validateOnChange: true,
-      }
-    );
-    const quantity = defineInputBinds(
-      { value: "quantity" },
-      { validateOnChange: true }
-    );
-    const costPrice = defineInputBinds(
-      { value: "costPrice" },
-      { validateOnChange: true }
-    );
-    const dateReceived = defineInputBinds(
-      { value: "dateReceived" },
-      { validateOnChange: true }
-    );
-    const onSubmit = () => {};
+    const name = defineInputBinds("name", { validateOnChange: true });
+    const model = defineInputBinds("model", {
+      validateOnChange: true,
+    });
+    const quantity = defineInputBinds("quantity", { validateOnChange: true });
+    const costPrice = defineInputBinds("costPrice", { validateOnChange: true });
+    const dateReceived = defineInputBinds("dateReceived", {
+      validateOnChange: true,
+    });
+    const onSubmit = async () => {
+      const form = await validate();
+    };
     return {
       name,
       model,
