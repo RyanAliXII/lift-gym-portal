ce284a69d019e0dc13ac6ef9ac259a2a7a4bce6a
Ryan Ali <ryanali456@gmail.com>
2023-10-11 17:24:07 +0800

    initialize permission selection

--- handlers/role.handler.go
+++ handlers/role.handler.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"lift-fitness-gym/app/pkg/acl"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -13,11 +14,10 @@ func (h *RoleHandler) RenderRolePage(c echo.Context) error {
 
 	return c.Render(http.StatusOK, "admin/role/main", Data{
 		"csrf" : c.Get("csrf"),
+		"permissions": acl.Permissions,
 	})
 }
 
 func NewRoleHandler () RoleHandler {
-	return RoleHandler{
-		
-	}
+	return RoleHandler{}
 }
--- views/admin/role/main.js
+++ views/admin/role/main.js
@@ -1,2 +1,34 @@
+const { createApp, onMounted, ref } = require("vue");
+import { useForm } from "@vorms/core";
+import Choices from "choices.js";
+createApp({
+  setup() {
+    const addSelectElement = ref(null);
+    const addSelect = ref(null);
+    const { register, handleSubmit, handleReset, errors } = useForm({
+      initialValues: {
+        name: "",
+        permissions: [],
+      },
+      onSubmit(data) {
+        console.log(data);
+      },
+    });
 
+    const { value: name, attrs: nameAttrs } = register("name");
+    const { value: permissions, attrs: permissionAttrs } =
+      register("permissions");
 
+    onMounted(() => {
+      addSelect.value = new Choices(addSelectElement.value, {
+        allowHTML: true,
+      });
+    });
+    return {
+      addSelectElement,
+      name,
+      permissions,
+      errors,
+    };
+  },
+}).mount("#RolePage");
