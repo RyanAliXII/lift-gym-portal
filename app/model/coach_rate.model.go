28cd98ecbf5055e287769d4189d4dac6924c0ea7
Ryan Ali <ryanali456@gmail.com>
2023-10-14 21:07:05 +0800

    create a model for coach rate

---
+++ app/model/coach_rate.model.go
@@ -0,0 +1,18 @@
+package model
+
+import validation "github.com/go-ozzo/ozzo-validation"
+
+type CoachRate struct {
+	Id          int     `json:"id"`
+	Description string  `json:"description" db:"description"`
+	Price       float64 `json:"price" db:"price"`
+	Model
+}
+
+func (m CoachRate) Validate() (error, map[string]string) {
+
+	return m.ValidationRules(&m, 
+		validation.Field(&m.Description, validation.Required.Error("Description is required."), validation.Length(1, 255).Error("Description should be atleast 1 to 255 characters.")),
+		validation.Field(&m.Price, validation.Required.Error("Price is required."), validation.Min(float64(1)).Error("Price cannot be less than or equal to 0")),
+	)
+}
