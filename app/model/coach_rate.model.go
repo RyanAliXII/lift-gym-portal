c60c8f520470f04efabf4dba6cdab5547a4e7989
Ryan Ali <ryanali456@gmail.com>
2023-10-14 21:08:26 +0800

    add coach id in coachrate model

--- app/model/coach_rate.model.go
+++ app/model/coach_rate.model.go
@@ -6,11 +6,11 @@ type CoachRate struct {
 	Id          int     `json:"id"`
 	Description string  `json:"description" db:"description"`
 	Price       float64 `json:"price" db:"price"`
+	CoachId     float64 `json:"coachId" db:"coach_id"`
 	Model
 }
 
 func (m CoachRate) Validate() (error, map[string]string) {
-
 	return m.ValidationRules(&m, 
 		validation.Field(&m.Description, validation.Required.Error("Description is required."), validation.Length(1, 255).Error("Description should be atleast 1 to 255 characters.")),
 		validation.Field(&m.Price, validation.Required.Error("Price is required."), validation.Min(float64(1)).Error("Price cannot be less than or equal to 0")),
