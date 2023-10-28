91e036c41e5b062f63a54c75b4f7d7d5d2db9575
Ryan Ali <Ryanali456@gmail.com>
2023-10-28 12:42:37 +0800

    render calendarjs

--- views/client/reservation/main.js
+++ views/client/reservation/main.js
@@ -1,7 +1,31 @@
-import { createApp, onMounted } from "vue";
-
+import { createApp, onMounted, ref } from "vue";
+import { Calendar } from "fullcalendar";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
 createApp({
   setup() {
-    onMounted(() => {});
+    const reservationCalendarElement = ref(null);
+    const reservationCalendar = ref(null);
+    onMounted(() => {
+      reservationCalendar.value = new Calendar(
+        reservationCalendarElement.value,
+        {
+          initialView: "dayGridMonth",
+          plugins: [timeGridPlugin, interactionPlugin],
+          height: "650px",
+          selectable: true,
+          allDaySlot: false,
+          headerToolbar: {
+            left: "prev,next",
+            right: "dayGridMonth,timeGridDay", // user can switch between the two
+          },
+          dateClick: (info) => {
+            reservationCalendar.value.changeView("timeGridDay", info.dateStr);
+          },
+        }
+      );
+      reservationCalendar.value.render();
+    });
+    return { reservationCalendarElement };
   },
 }).mount("#ReservationPage");
