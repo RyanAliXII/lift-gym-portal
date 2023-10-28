43c25995955c5bb92ca3a192323d84423c326884
Ryan Ali <Ryanali456@gmail.com>
2023-10-28 11:47:26 +0800

    create handler and page for client reservation

---
+++ handlers/reservation.handler.go
@@ -0,0 +1,20 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+
+type ReservationHandler struct {
+
+}
+func NewReservationHandler () ReservationHandler{
+	return ReservationHandler{}
+}
+
+
+func(h * ReservationHandler) RenderClientReservationPage(c echo.Context) error {
+	return c.Render(http.StatusOK, "client/reservation/main", Data{})
+}
